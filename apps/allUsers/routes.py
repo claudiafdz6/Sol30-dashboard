c1fd10ee53c3ff8181730aa0f76b6934abec738d
Claudia Fernandez <claudia.fernandez-XT@solutions30italia.it>
2024-06-10 09:57:26 +0200

    created users page, changed visibility of the sections depending  if it's an user or admin on the sidebar

--- apps/allUsers/routes.py
+++ apps/allUsers/routes.py
@@ -1,13 +1,20 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from apps import db
-from apps.tickets.forms import TicketForm
+from apps.allUsers.forms import TicketForm
 from apps.authentication.models import TicketSupervisor
 
-blueprint = Blueprint('tickets_blueprint', __name__)
+blueprint = Blueprint('allUsers_blueprint', __name__)
 
-@blueprint.route('/tickets', methods=['GET', 'POST'])
+@blueprint.route('/allUsers', methods=['GET', 'POST'])
 @login_required
+def allUsers():
+    form = allUsers()
+    if request.method == 'POST':
+        if current_user.is_admin:
+            show_allUsers = UserInformations()
+
+
 def tickets():
     form = TicketForm()
     if request.method == 'POST':
@@ -18,18 +25,16 @@ def tickets():
                 id_task=form.id_task.data,
                 note=form.note.data,
                 tag=form.tag.data,
-                # data_apertura=form.data_apertura.data,  # <-- default=datetime.utcnow su model
-                # data_chiusura=form.data_chiusura.data # <-- aggiorniamo la data alla chiusura
             )
             db.session.add(new_ticket)
             db.session.commit()
-            flash('Ticket added', 'success')
+            flash('The data has been modify', 'success')
         else:
-            flash('You are not authorized to add tickets.', 'danger')
-        return redirect(url_for('tickets_blueprint.tickets'))
+            flash('You are not authorized to modify these data.', 'danger')
+        return redirect(url_for('allUsers_blueprint.tickets'))
     
 
     if request.method == 'GET':
         tickets = TicketSupervisor.query.all()
-        return render_template('home/index.html', tickets=tickets, form=form)
+        return render_template('home/allUsers.html', tickets=tickets, form=form)
  
